ca0f784dcb841c8f5e92bd2c065278fbfaf20fe0
SereenElghazali <34245211+SereenElghazali@users.noreply.github.com>
2022-08-16 14:59:59 +0300

    Update LiveCasinoHistoryDetailPopup.jsx

--- src/components/myPage/popups/LiveCasinoHistoryDetailPopup.jsx
+++ src/components/myPage/popups/LiveCasinoHistoryDetailPopup.jsx
@@ -4,13 +4,16 @@ import Sample1 from '../../../images/myPage/betHistory/sample1_v2.png'
 import Sample2 from '../../../images/myPage/betHistory/sample2_v2.png'
 
 const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
-    return (
-        <div 
-            style={{ height: "793px", width: "690px", borderRadius: "10px", backgroundColor: 'rgb(50, 50, 49)', boxShadow: '3.5px 6.062px 7px 0px rgba(0, 0, 0, 0.75)' ,borderWidth:'1px', borderColor:'#1f1f1e'}}
-            className="bg-white overflow-hidden"
+  return (
+      <div 
+        style={{ height: "793px",width: "690px", borderRadius: "10px", background:"linear-gradient(to top, #1f1f1e 80%, #343434 100%)",borderRadius: "10px",boxShadow: '3.5px 6.062px 7px 0px rgba(0, 0, 0, 0.75)'}}
+        className="overflow-hidden p-px"
         >
-          
-            <div className="relative h-65px bg-gray-272726 flex items-center justify-center flex-shrink-0">
+        <div 
+            style={{backgroundColor: 'rgb(50, 50, 49)' }}
+            className="h-full overflow-hidden"
+        >
+            <div className="relative h-61px bg-gray-272726 flex items-center justify-center flex-shrink-0">
                 <label className="font-spoqaBold text-yellow-ad9e8c tracking-tight text-24px">거래 상세 내역</label>
                 <button className="absolute right-0 mr-26px cursor-pointer z-20" onClick={()=> setPopupOpen(false)}>
                 <img src={CloseIcon} alt="close_icon" />
@@ -27,14 +30,14 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                 {/* <div style={{width:"459px", backgroundColor:"#868686"}} className="h-px mb-22px"></div> */}
 
                 <div 
-                    style={{width:"618px", height:"619px", border:'2px solid #473e35',boxShadow: '0px 1px 4px 0px rgba(0, 0, 0, 0.5)'}} 
+                    style={{width:"618px", height:"620px", border:'2px solid #473e35',boxShadow: '0px 1px 4px 0px rgba(0, 0, 0, 0.5)'}} 
                     className="rounded-6px overflow-hidden"
                 >
  
                     <div className="flex flex-col">
                         <div 
-                            style={{backgroundColor:"#473e35"}}
-                            className="h-42px flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight overflow-hidden"
+                            style={{backgroundColor:"#473e35",height:"41px"}}
+                            className="flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight overflow-hidden"
                         >
                             바카라 on 2021-08-09 17:26:30
                         </div>
@@ -46,47 +49,47 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                             >
                                 <div 
                                     style={{height:"41px"}} 
-                                    className="h-41px border-b border-black flex items-center justify-center"
+                                    className="border-b border-black flex items-center justify-center"
                                 >
                                     게임
                                 </div>
                                 <div 
                                     style={{height:"41px"}} 
-                                    className="h-41px border-b border-black flex items-center justify-center"
+                                    className="border-b border-black flex items-center justify-center"
                                 >
                                     테이블아이디
                                 </div>
                                 <div 
                                     style={{height:"41px"}} 
-                                    className="h-41px border-b border-black flex items-center justify-center"
+                                    className="border-b border-black flex items-center justify-center"
                                 >
                                     게임상태
                                 </div>
                                 <div 
                                     style={{height:"41px"}} 
-                                    className="h-41px flex items-center justify-center"
+                                    className="flex items-center justify-center"
                                 >
                                     종료시간
                                 </div>
 
                                 <div 
                                     style={{backgroundColor:"#473e35"}}
-                                    className="h-42px flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight"
+                                    className="h-40px flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight"
                                 >
                                     뱅커 핸드
                                 </div>
 
                                 <div 
-                                    style={{height:"127px"}} 
-                                    className="  flex items-center justify-center bg-gray-2b2b2a"
+                                    style={{height:"128px"}} 
+                                    className="flex items-center justify-center bg-gray-2b2b2a"
                                 >
                                     <img src={Sample1} alt="" />
                                 </div>
 
-                                <div style={{height:"42px"}} className="flex h-41px border-b border-dark-252525">
+                                <div style={{height:"41px"}} className="flex border-b border-dark-252525">
                                     <div 
                                         style={{backgroundColor:"#473e35", width:"71px"}}
-                                        className=" flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight border-r border-black"
+                                        className="flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight border-r border-black"
                                     >
                                         베팅
                                     </div>
@@ -158,19 +161,19 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
 
                                 <div 
                                     style={{backgroundColor:"#473e35"}}
-                                    className="h-42px flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight"
+                                    className="h-40px flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight"
                                 >
                                     플레이어 핸드
                                 </div>
 
                                 <div 
-                                    style={{height:"127px"}} 
+                                    style={{height:"128px"}} 
                                     className="flex items-center justify-center"
                                 >
                                     <img src={Sample2} alt="" />
                                 </div>
 
-                                <div style={{height:"41px"}}  className="flex">
+                                <div style={{height:"40px"}}  className="flex">
                                     <div 
                                         style={{backgroundColor:"#473e35"}}
                                         className=" flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight  w-full "
@@ -186,7 +189,7 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                                 </div>
 
 
-                                <div style={{height:"42px"}} className="flex font-robotoRegular text-gray-c8c8c8">
+                                <div style={{height:"41px"}} className="flex font-robotoRegular text-gray-c8c8c8">
                                     <div 
                                         style={{}}
                                         className=" flex items-center justify-center text-16px tracking-tight border-r w-full border-black bg-gray-2b2b2a"
@@ -205,7 +208,7 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                         </div>
 
                         <div 
-                            style={{backgroundColor:"#473e35", height: "41px"}}
+                            style={{backgroundColor:"#473e35", height: "40px"}}
                             className="flex items-center justify-center text-16px font-spoqaMedium tracking-tight text-golden-highLight"
                         >
                             게임요약
@@ -216,20 +219,20 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                                 className="flex flex-col w-full border-r border-black text-16px font-spoqaMedium tracking-tight text-gray-c8c8c8"
                             >
                                 <div 
-                                    style={{height:"41px"}} 
+                                    style={{height:"42px"}} 
                                     className="h-41px border-b border-black flex items-center justify-center"
                                 >
                                     총 베팅금액
                                 </div>
                                 <div 
-                                    style={{height:"41px"}} 
+                                    style={{height:"40px"}} 
                                     className="h-41px border-b border-black flex items-center justify-center"
                                 >
                                     총 지불금액
                                 </div>
                                 <div 
-                                    style={{height:"41px"}} 
-                                    className="h-41px border-b border-black flex items-center justify-center"
+                                    style={{height:"39px"}} 
+                                    className="flex items-center justify-center"
                                 >
                                     총 회원승패
                                 </div>
@@ -239,20 +242,20 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                                 className="flex flex-col w-full  text-16px font-robotoRegular tracking-tight text-gray-c8c8c8"
                             >
                                 <div 
-                                    style={{height:"41px"}} 
+                                    style={{height:"42px"}} 
                                     className="h-41px border-b border-black flex items-center justify-end pr-15px"
                                 >
                                     5,000.00
                                 </div>
                                 <div 
-                                    style={{height:"41px"}} 
+                                    style={{height:"40px"}} 
                                     className="h-41px border-b border-black flex items-center justify-end pr-15px"
                                 >
                                     10,000.00
                                 </div>
                                 <div 
-                                    style={{height:"41px"}} 
-                                    className="h-41px border-b border-black flex items-center justify-end pr-15px"
+                                    style={{height:"40px"}} 
+                                    className="flex items-center justify-end pr-15px"
                                 >
                                     5,000.00
                                 </div>
@@ -262,7 +265,7 @@ const LiveCasinoHistoryDetailPopup = ({setPopupOpen}) => {
                 </div>
             </div>
 
-            
+            </div>
         </div>
     )
 }
