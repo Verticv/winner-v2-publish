a9ff131093410a1af7c7a004f351d2c1d0b2410e
동해 <andrew@donghaeui-Macmini.local>
2022-12-19 14:56:17 +0900

    .

--- src/components/myPage/tables/InboxTable.jsx
+++ src/components/myPage/tables/InboxTable.jsx
@@ -85,7 +85,7 @@ const InboxTable = ({ array, checkedState, setCheckedState }) => {
               {item.text}
             </p>
             {item.isRead === false && (
-              <div className="w-17px h-17px bg-red-e9441d rounded-4px text-12px text-white flex items-center justify-center font-roboto flex-shrink-0 pt-px">
+              <div className="w-17px h-17px bg-red-e9441d rounded-4px text-12px text-white flex items-center justify-center font-roboto flex-shrink-0 pt-px -mt-px">
                 <p className="flex items-center h-12px -ml-px">N</p>
               </div>
             )}
